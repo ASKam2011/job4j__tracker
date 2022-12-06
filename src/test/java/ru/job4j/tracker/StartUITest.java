283f2636a9b38edcdab89816d576d4569be18744
Admin <20nastya93@mail.ru>
2022-12-06 16:56:44 +0700

    5.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -26,4 +26,42 @@ class StartUITest {
         Item expected = new Item("Новая заявка");
         assertThat(created.getName()).isEqualTo(expected.getName());
     }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @Test
+    public void checkingEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Новая заявка");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "Измененная заявка"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("Измененная заявка");
+    }
+
+    @Test
+    public void checkingDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Удаление заявки");
+        tracker.add(item);
+        int id = item.getId();
+        tracker.delete(id);
+        assertThat(tracker.findById(id)).isNull();
+    }
 }
