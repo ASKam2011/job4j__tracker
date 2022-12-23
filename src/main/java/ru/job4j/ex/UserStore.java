424f0bc5f2baf970409ddda66588574dcf18ab8d
Admin <20nastya93@mail.ru>
2022-12-23 11:44:38 +0700

    0.1. java.lang.NullPointerException [#219364 #370841]

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,41 @@
+package ru.job4j.ex;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User rsl = null;
+        for (User user : users) {
+            if (login.equals(user.getUsername())) {
+                rsl = user;
+                System.out.println(rsl);
+            }
+        }
+            if (rsl == null) {
+                throw new UserNotFoundException("Пользователь не найден.");
+            }
+        return rsl;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        boolean rsl = true;
+        if (!user.isValid() && user.getUsername().length() < 3) {
+            throw new UserInvalidException("Пользователь не валидный.");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            User[] users = {
+                    new User("Анастасия", true)
+            };
+            User user = findUser(users, "Анастасия");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException ui) {
+            ui.printStackTrace();
+        } catch (UserNotFoundException un) {
+            un.printStackTrace();
+        }
+    }
+}
